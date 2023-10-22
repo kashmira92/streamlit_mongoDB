c1fc2297c558d5177fd153ffeb428c528d29b1ce
Kashmira Golatkar <48323327+kashmira92@users.noreply.github.com>
2023-10-21 23:19:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
 # --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
-years = [datetime.today().year-2, datetime.today().year -1 ,datetime.today().year, datetime.today().year + 1]
+years = [,datetime.today().year, datetime.today().year-2, datetime.today().year -1, datetime.today().year + 1]
 months = list(calendar.month_name[1:])
 
 # --- DATABASE INTERFACE ---
